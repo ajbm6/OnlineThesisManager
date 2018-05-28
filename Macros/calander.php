1bb0204d8294d760478e58f94ed31c4c84c189e5
ajbm6 <ajbm6@users.noreply.github.com>
2018-05-29 00:10:13 +0800

    Update calander.php

--- Macros/calander.php
+++ Macros/calander.php
@@ -1,35 +1,30 @@
 <?php
-      function result_query($X)
-            {
-                ini_set('max_execution_time', 300);
-      if (!($con = mysqli_connect(constant("HOSTNAME"), constant("USERNAME"), constant("PASS")))) {
+
+function result_query($X)
+{
+    ini_set('max_execution_time', 300);
+    
+    if (!($con = mysqli_connect(constant("HOSTNAME"), constant("USERNAME"), constant("PASS")))) {
         $result = "DBCONNECTION_ERROR";
-    } else if (!($select = mysqli_select_db($con,constant("DBNAME")))) {
-        $result = "DBCONNECTION_ERROR";
-    } else {
-            
-                        $sql=mysqli_query($con,$X);
-                        
-                    $row=mysqli_fetch_array($sql); 
-                            return $row;
-                            
-            }
-            }
- ?>
- <?php
-        function bool_result_query($X)
-        {
-            ini_set('max_execution_time', 300);
-      if (!($con = mysqli_connect(constant("HOSTNAME"), constant("USERNAME"), constant("PASS")))) {
-        $result = "DBCONNECTION_ERROR";
-    } else if (!($select = mysqli_select_db($con,constant("DBNAME")))) {
+    } elseif (!($select = mysqli_select_db($con,constant("DBNAME")))) {
         $result = "DBCONNECTION_ERROR";
     } else {
-            
-                        $sql=mysqli_query($con,$X);
-                        
-                    return($sql);
-                            
-            }
-        }
-?>
+        $sql = mysqli_query($con, $X);
+        $row = mysqli_fetch_array($sql); 
+        
+        return $row;
+    }
+}
+
+function bool_result_query($X)
+{
+    ini_set('max_execution_time', 300);
+    if (!($con = mysqli_connect(constant("HOSTNAME"), constant("USERNAME"), constant("PASS")))) {
+        $result = "DBCONNECTION_ERROR";
+    } elseif (!($select = mysqli_select_db($con,constant("DBNAME")))) {
+        $result = "DBCONNECTION_ERROR";
+    } else {
+        $sql = mysqli_query($con,$X);
+        return($sql);
+    }
+}
