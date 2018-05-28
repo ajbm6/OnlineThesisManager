d9f7858eea85ace4219e17b476cf17e889352d77
ajbm6 <ajbm6@users.noreply.github.com>
2018-05-29 00:25:57 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -11,6 +11,35 @@ define("USERNAME", "root");
 define("PASS", "");
 define("DBNAME", "project");
 
+class Config
+{
+    private $conn;
+    
+    public function __construct()
+    {
+        if (!$this->isConnected()) {
+            $this->connect();
+        }
+    }
+    
+    public function connect()
+    {
+        $this->conn = new \mysqli('localhost', 'root', '', 'project');
+        
+        if ($this->conn->connect_error) {
+			throw new \Exception('Error: ' . $this->conn->error . '<br />Error No: ' . $this->conn->errno);
+		}
+        
+		$this->conn->set_charset("utf8");
+		$this->conn->query("SET SQL_MODE = ''");
+    }
+    
+    public function isConnected()
+    {
+        return $this->conn->ping();
+    }
+}
+
 if (function_exists('base_path')) {
     function base_path($path = '')
     {
